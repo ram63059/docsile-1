63ae28e6619649542894afad3a35a49743b8b4ab
ram63059 <21bsm038@iiitdmj.ac.in>
2025-01-31 11:13:45 +0530

    Update QuestionDetail.tsx

--- frontend/src/pages/QuestionDetail.tsx
+++ frontend/src/pages/QuestionDetail.tsx
@@ -781,7 +781,7 @@ const QuestionDetail: React.FC = () => {
         </div>
 
         {/* Related Questions Sidebar */}
-        <div className="w-80 shrink-0">
+        <div className="hidden lg:block w-80 shrink-0">
           <div className="bg-white rounded-lg shadow p-4">
             <div className="flex justify-between items-center mb-4">
               <h3 className="font-medium text-main  ">Explore Related Questions</h3>
